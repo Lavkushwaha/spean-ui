f5672332f8229f07f72669a4d62bbfb00693250b
Lavkant Kachhwaha <kush373@gmail.com>
2022-02-14 15:07:45 +0530

    homescreen changes added

--- lib/modules/home_screen/home_screen.dart
+++ lib/modules/home_screen/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:spean_test/modules/home_screen/components/horizontal_card.dart';
+import 'package:spean_test/modules/home_screen/components/vertical_card.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({
@@ -10,38 +12,99 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  final ScrollController _scrollController = ScrollController();
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  final verticalList = List<Widget>.generate(
+      10, (int index) => const VerticalCard(),
+      growable: true);
+
+  getHeader({title}) {
+    return Container(
+      width: double.infinity,
+      height: 50,
+      child: Center(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              '$title',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('ss'),
+        elevation: 0,
+        backgroundColor: Colors.white,
+        centerTitle: false,
+        title: const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          child: Text(
+            'Flutter Test',
+            style: TextStyle(
+              color: Colors.black,
+            ),
+          ),
+        ),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: ListView(
+          scrollDirection: Axis.vertical,
+          controller: _scrollController,
+          children: [
+            getHeader(title: "Vertical List Header"),
+            ...verticalList,
+            getHeader(title: "Horizontal List Header"),
+            SizedBox(
+              height: 80,
+              child: ListView.builder(
+                  itemCount: 10,
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context, index) {
+                    return const HorizontalCard();
+                  }),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            getHeader(title: "Vertical List Header"),
+            ...verticalList,
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      bottomNavigationBar: GestureDetector(
+        onTap: () {
+          _scrollController.animateTo(0,
+              duration: Duration(milliseconds: 1000), curve: Curves.easeIn);
+        },
+        child: Container(
+          height: 100,
+          decoration: const BoxDecoration(
+            color: Colors.white,
+            border: Border(
+              top: BorderSide(
+                color: Colors.black,
+                width: 1,
+              ),
+            ),
+          ),
+          child: const Center(
+            child: Text(
+              'Scroll to top',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
